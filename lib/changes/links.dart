f0916733c30a192f0d6d4ada550c85e068199813
Dipendra-Raghav <raghavdip19@gmail.com>
2023-05-23 14:20:29 +0530

    PortFolio website done

---
+++ lib/changes/links.dart
@@ -0,0 +1,35 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1YcL0hHdGVJye7tR2fmhSLUZkWmfwVb-t/view';
+
+// Github Link
+const String gitHub = 'https://github.com/Dipendra-Raghav';
+// Github Link
+
+// Github Link
+const String twitter = 'https://twitter.com/_Dipendra_R';
+
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/dipendra-raghav/';
+// Github Link
+// Github Link
+const String instagram = 'https://www.instagram.com/dipendra.raghav_/';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+
+// Github Link
+
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=9149075607';
